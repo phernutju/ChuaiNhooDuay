b730bdb48760be326300a2946cc54c3f368a0806
t_t <chotima.eamch@mail.kmutt.ac.th>
2026-06-03 23:58:18 +0700

    merge noti&map

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "com.example.we_are_ready"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23 // local_auth requires API 23+
+        minSdk = flutter.minSdkVersion // local_auth requires API 23+
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -48,6 +48,39 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+
+    packaging {
+        resources {
+            excludes += setOf(
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/ASL2.0",
+                "META-INF/*.kotlin_module",
+                "META-INF/versions/**",
+                "google/protobuf/*.proto",
+                "META-INF/proguard/*.pro",
+                "javax.annotation/**",
+                "META-INF/com.android.tools/**",
+            )
+            pickFirsts += setOf(
+                "META-INF/services/*",
+                "protobuf.meta",
+                "google/protobuf/**",
+            )
+        }
+    }
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("com.google.guava:guava:32.1.3-android")
+        force("com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava")
+        force("com.google.j2objc:j2objc-annotations:2.8")
+        force("com.google.code.findbugs:jsr305:3.0.2")
     }
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,9 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_storage
+import flutter_secure_storage_macos
 import geolocator_apple
+import local_auth_darwin
 import sqflite_darwin
 import url_launcher_macos
 
@@ -20,7 +22,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
